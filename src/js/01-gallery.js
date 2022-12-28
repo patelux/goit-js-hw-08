75be8409486736a74c1163ba9218aee16ca3535b
Natalia Levenets <levenets.lot@gmail.com>
2022-12-29 01:49:30 +0200

    tasks 1 2 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+let items = galleryItems.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
+}).join('');
+
+galleryBox.insertAdjacentHTML('beforeend', items);
+
+var images = new SimpleLightbox('.gallery a');
+images.on('show.simplelightbox', function () {
+    images.options.captionsData = "alt";
+    images.options.captionsDelay = 250;
+    images.options.scrollZoom = false;
+    images.options.scrollZoomFactor = 0;
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const arr = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+}
+
+arr.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+    const formData = JSON.stringify({ email: arr.email.ariaValueMax, message: arr.message.value });
+    localStorage.setItem(LOCALSTORAGE_KEY, formData);
+}
+
+arr.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    // console.log(event.target.email.value);
+    if (!event.target.email.value  || !event.target.message.value) {
+        alert('Заповніть усі поля форми');
+        return;        
+    }
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+    event.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+dataFromLocalStorage();
+
+function dataFromLocalStorage() {
+    const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (data) {
+        email.value = data.email;
+        message.value = data.message;
+    }
+}
